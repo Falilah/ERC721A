ece4d18fb6b4d3ef9111804a1a27a833a9d492ca
falilah <leogoldf21@gmail.com>
2022-07-06 07:45:51 +0100

    attempted task

---
+++ merkleScript/tree.js
@@ -0,0 +1,35 @@
+const { MerkleTree } = require("merkletreejs");
+const keccak256 = require("keccak256");
+let add1 = "0x0000000000000000000000000000000000000001";
+let add2 = "0x0000000000000000000000000000000000000002";
+let add3 = "0x0000000000000000000000000000000000000003";
+let add4 = "0x0000000000000000000000000000000000000004";
+
+let addresses = [add1, add2, add3, add4];
+
+//hash leaves
+let leaves = addresses.map((x) => keccak256(x));
+//create a tree
+let merkletree = new MerkleTree(leaves, keccak256, {
+  sortPairs: true,
+  sortLeaves: true,
+});
+console.log(merkletree.toString());
+
+//get roothasheh
+let root = merkletree.getHexRoot().toString("hex");
+console.log("root is :", root);
+
+let address1 = leaves[0];
+// console.log(address1.toString("hex"));
+
+let whitelist1 = keccak256(address1);
+// console.log(whitelist1.toString("hex"));
+
+//get proof of the first element
+let proof = merkletree.getHexProof(whitelist1);
+console.log("the proof for whitelist 1 is:", proof);
+
+//verify an address
+let verify = merkletree.verify(proof, whitelist1, root);
+console.log(verify);
--- src/test/drm.t.sol
+++ src/test/drm.t.sol
@@ -20,74 +20,87 @@ contract DreamersTest is Test {
 
     }
 
-    function testendpRESaleAndSetupPublicSale() public{
-        dreamer.endPreSaleAndSetupPublicSale(1656418579, 5 ether, 5);
-        // assertEq(dreamer.publicSaleProps.price, 5 ether);
+    // function testendpRESaleAndSetupPublicSale() public{
+    //     dreamer.endPreSaleAndSetupPublicSale(1656418579, 5 ether, 5);
+    //     // assertEq(dreamer.publicSaleProps.price, 5 ether);
 
         
-    }
-    function testPublicSale() public{
-        vm.startPrank(bob);
-        dreamer = new DRM();
-        dreamer.endPreSaleAndSetupPublicSale(1756421501, 2 ether, 5);
-        dreamer.setBaseURI("ipfs://QmcKBFgWTEayKPrKhaQhz4cmAqjzzhncztpyJS44YyF4dv/");
+    // }
 
-        deal(bob, 20 ether);
-        vm.warp(1756421907);
 
-        dreamer.publicSaleMint{value:5 ether}(2);
+    // function testPublicSale() public{
+    //     vm.startPrank(bob);
+    //     dreamer = new DRM();
+    //     dreamer.endPreSaleAndSetupPublicSale(1756421501, 2 ether, 5);
+    //     dreamer.setBaseURI("ipfs://QmcKBFgWTEayKPrKhaQhz4cmAqjzzhncztpyJS44YyF4dv/");
 
-        uint val = dreamer.totalSupply();
-        assertEq(val, 2);
+    //     deal(bob, 20 ether);
+    //     vm.warp(1756421907);
 
-        string memory name = dreamer.tokenURI(0);
-        assertEq(name,"ipfs://QmcKBFgWTEayKPrKhaQhz4cmAqjzzhncztpyJS44YyF4dv/0.json");
+    //     dreamer.publicSaleMint{value:5 ether}(2);
 
-        uint minted = dreamer.numberMinted(bob);
-         assertEq(minted, 2);   
+    //     uint val = dreamer.totalSupply();
+    //     assertEq(val, 2);
+
+    //     string memory name = dreamer.tokenURI(0);
+    //     assertEq(name,"ipfs://QmcKBFgWTEayKPrKhaQhz4cmAqjzzhncztpyJS44YyF4dv/0.json");
+
+    //     uint minted = dreamer.numberMinted(bob);
+    //      assertEq(minted, 2);   
     
-        assertEq(bob.balance, 16 ether);
-        assertEq(address(dreamer).balance, 4 ether);
+    //     assertEq(bob.balance, 16 ether);
+    //     assertEq(address(dreamer).balance, 4 ether);
 
 
-        dreamer.withdrawMoney();
-        assertEq(address(dreamer).balance, 0 ether);
-        assertEq(bob.balance, 20 ether);
+    //     dreamer.withdrawMoney();
+    //     assertEq(address(dreamer).balance, 0 ether);
+    //     assertEq(bob.balance, 20 ether);
         
 
 
 
-        vm.stopPrank();
+    //     vm.stopPrank();
 
 
 
-    }
-    function testTeamMint() public {
-        dreamer.addTeamMember(alice);
-        dreamer.addTeamMember(bob);
-        dreamer.addTeamMember(mary);
-        vm.startPrank(bob);
-        dreamer.teamMint();
+    // }
+    // function testTeamMint() public {
+    //     dreamer.addTeamMember(alice);
+    //     dreamer.addTeamMember(bob);
+    //     dreamer.addTeamMember(mary);
+    //     vm.startPrank(bob);
+    //     dreamer.teamMint();
+    //     assertEq(dreamer.numberMinted(bob), 5);
+    //     vm.stopPrank();
+    //      vm.startPrank(alice);
+    //     dreamer.teamMint();
+    //     assertEq(dreamer.numberMinted(alice), 5);
+    //     dreamer.tokenURI(6);
+    //     vm.expectRevert("teamMint: you have previously minted your NFT");
+    //     dreamer.teamMint();
+    //     vm.expectRevert("Ownable: caller is not the owner");
+    //     dreamer.addTeamMember(rose);
+    //     vm.stopPrank();
+    //     vm.prank(rose);
+    //     vm.expectRevert("not a member on the team");
+    //     dreamer.teamMint();
+
+
+
+
+
+
+    // }
+
+    function testpresaleMint() public {
+        
+
+        dreamer.setPresaleProps();
+         vm.startPrank(bob);
+         deal(bob, 20 ether);
+        dreamer.presaleMint(_merkleProof, quantity);
         assertEq(dreamer.numberMinted(bob), 5);
         vm.stopPrank();
-         vm.startPrank(alice);
-        dreamer.teamMint();
-        assertEq(dreamer.numberMinted(alice), 5);
-        dreamer.tokenURI(6);
-        vm.expectRevert("teamMint: you have previously minted your NFT");
-        dreamer.teamMint();
-        vm.expectRevert("Ownable: caller is not the owner");
-        dreamer.addTeamMember(rose);
-        vm.stopPrank();
-        vm.prank(rose);
-        vm.expectRevert("not a member on the team");
-        dreamer.teamMint();
-
-
-
-
-
-
     }
 
 
